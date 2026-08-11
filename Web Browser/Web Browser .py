c15eb931120beb851c49bf077acf770d2b46bf56
Adoniel Sarkisian <clarence.sarkisian@gmail.com>
2026-08-11 09:46:52 +0330

    Web Browser

---
+++ Web Browser/Web Browser .py
@@ -0,0 +1,13 @@
+import webbrowser as wb
+
+pages = [
+    "https://chatgpt.com/",
+    "https://claude.ai/",
+    "https://manus.im/",
+    "https://perplexity.ai/", ]
+
+for i in pages:
+    wb.open(i)
+    
+    
+    
